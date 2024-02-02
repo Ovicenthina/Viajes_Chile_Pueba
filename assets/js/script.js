820845988bdbcde3469a630fc8c8b8946a49abca
Orihana Triana <ovicenthina25@gmail.com>
2024-02-02 17:45:31 -0300

    Agregando comentarios al proyecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-
+    //Función de Bootstrap para Tooltips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
     
+    //Función para generar un alert al presionar enviar con el evento click
     $("#enviarMensaje").first().click (function(){
         alert("El mensaje fue enviado correctamente")
     })
     
+    //Función para que en un intervalo de 5000' se cambie la foto del carrousel
     $('#carouselExampleAutoplaying').carousel({
         interval: 5000
     });
 
+    //Función con el evento click para generar un smooth scroll
     $("a").on('click', function(event) {
       
         if (this.hash !== "") {
